cc9939eba12772b096fb7c0cc456b097cd5893fb
PragatiAgrawal04 <105303281+PragatiAgrawal04@users.noreply.github.com>
2024-06-06 12:06:16 +0530

    Update OC.py

--- pages/OC.py
+++ pages/OC.py
@@ -101,10 +101,7 @@ def get_dataframe(ticker, exp_date_selected):
     main_url = "https://www.nseindia.com/"
     response = requests.get(main_url, headers=headers)
     cookies = response.cookies
-    if ticker == 'NIFTY':
-        url = f"https://www.nseindia.com/api/option-chain-indices?symbol={ticker}"
-    else:
-        url = f"https://www.nseindia.com/api/option-chain-equities?symbol={ticker}"
+    url = f"https://www.nseindia.com/api/option-chain-equities?symbol={ticker}"
     option_chain_data = requests.get(url, headers=headers, cookies=cookies)
     data = option_chain_data.json()["records"]["data"]
     ocdata = []
