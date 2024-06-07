ad27a4071a77af3aa7404d30b1b6ff83ff5478e6
PragatiAgrawal04 <105303281+PragatiAgrawal04@users.noreply.github.com>
2024-06-07 12:10:12 +0530

    Update OC.py

--- pages/OC.py
+++ pages/OC.py
@@ -254,7 +254,8 @@ def frag_table(table_number, selected_option='UBL', exp_option=EXP_OPTION):
             ########################################## Stock LTP and Matrix #######################################
             stock_ltp = current_market_price(ticker)
             low_52_week, high_52_week = fifty_two_week_high_low(ticker, exchange)
-    
+
+    st.divider()
     d1, d2, d3, d4 = st.columns(4)
     with d1:
         st.markdown('##### CMP:  ' + str(stock_ltp))
@@ -264,6 +265,7 @@ def frag_table(table_number, selected_option='UBL', exp_option=EXP_OPTION):
         st.markdown('##### 52 week low:  ' + str(low_52_week))
     with d4:
         st.markdown('##### 52 week high:  ' + str(high_52_week))
+    st.divider()
     
     output_ce = output_ce[
         ['strikePrice', 'pchangeinOpenInterest', 'pChange', 'totalTradedVolume', 'impliedVolatility', 'lastPrice',
