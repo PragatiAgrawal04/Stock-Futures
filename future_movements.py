ab05bf860169fb0e8f533755d0445c80ddfad96b
PragatiAgrawal04 <105303281+PragatiAgrawal04@users.noreply.github.com>
2024-06-18 14:29:28 +0530

    Update future_movements.py

--- future_movements.py
+++ future_movements.py
@@ -342,8 +342,8 @@ nift = extract_bhavcopy('FUTIDX')
 action_data_all_stk = pd.DataFrame()
 for i in stk_symbol_list:
     one_symbol_data = all_stocks_data.loc[(all_stocks_data['SYMBOL'] == i)].reset_index(drop=True)
-    one_stock_data = one_symbol_data.loc[one_symbol_data['EXPIRY_DT'] == list(one_symbol_data['EXPIRY_DT'].unique())[0]]
-    action_data_one_stk = action_setting(one_stock_data)
+    one_symbol_data = one_symbol_data.loc[one_symbol_data['EXPIRY_DT'] == list(one_symbol_data['EXPIRY_DT'].unique())[0]]
+    action_data_one_stk = action_setting(one_symbol_data)
     action_data_all_stk = pd.concat([action_data_all_stk, action_data_one_stk], ignore_index=True)
 
 # st.dataframe(action_data_all_stk)
