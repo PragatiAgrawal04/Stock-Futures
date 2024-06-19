da847bc015a33e4935362f42f1bae60313264171
PragatiAgrawal04 <105303281+PragatiAgrawal04@users.noreply.github.com>
2024-06-19 14:41:43 +0530

    Update future_movements.py

--- future_movements.py
+++ future_movements.py
@@ -343,7 +343,7 @@ action_data_all_stk = pd.DataFrame()
 for i in stk_symbol_list:
     one_symbol_data = all_stocks_data.loc[(all_stocks_data['SYMBOL'] == i)].reset_index(drop=True)
     eexp = list(set(list(one_symbol_data['EXPIRY_DT'])))[0]
-    print("-------------------",eexp)
+    print(i,"-------------------",eexp)
     #one_symbol_data = one_symbol_data.loc[one_symbol_data['EXPIRY_DT'] == ]
     action_data_one_stk = action_setting(one_symbol_data)
     action_data_all_stk = pd.concat([action_data_all_stk, action_data_one_stk], ignore_index=True)
